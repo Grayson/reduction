aa055bacadfba4099ecfb37b39ecd6596b83aabb
Grayson <info@fromconcentratesoftware.com>
2018-04-15 21:41:06 -0400

    Define basic converters

---
+++ library/src/header-converter.hpp
@@ -0,0 +1,6 @@
+#include "bridge.hpp"
+
+namespace reduction {
+	template <typename TDeductionClass>
+	bridge generate_c_wrapper_header(TDeductionClass const & klass);
+}
---
+++ library/src/implementation-converter.hpp
@@ -0,0 +1,6 @@
+#include "bridge.hpp"
+
+namespace reduction {
+	template <typename TDeductionClass>
+	bridge generate_c_wrapper_implementation(TDeductionClass const & klass);
+}
